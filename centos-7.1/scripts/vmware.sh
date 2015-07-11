179b9a733c66cc840a300686ac3274f0d088d4fc
Daniel Da Cunha <daniel@ddc.im>
2015-07-11 22:39:42 +0800

    Install VMWare Tools

--- centos-7.1/scripts/vmware.sh
+++ centos-7.1/scripts/vmware.sh
@@ -1,1 +1,18 @@
-yum install -y fuse-libs open-vm-tools
+#!/bin/bash
+
+# Make sure perl is available
+yum -y install perl
+yum -y install net-tools
+# Mount the disk image
+cd /tmp
+mkdir /tmp/isomount
+mount -t iso9660 -o loop /home/vagrant/linux.iso /tmp/isomount
+
+# Install the drivers
+cp /tmp/isomount/VMwareTools-*.gz /tmp
+tar -zxvf VMwareTools*.gz
+./vmware-tools-distrib/vmware-install.pl -d
+
+# Cleanup
+umount isomount
+rm -rf isomount /root/linux.iso VMwareTools*.gz vmware-tools-distrib
